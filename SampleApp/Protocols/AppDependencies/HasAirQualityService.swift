68a490e71122375bd635eb11dab1ac69e8ca51ae
Bartosz Kucharski <bkucharski.dev@gmail.com>
2018-04-30 19:34:43 +0200

    small change

--- SampleApp/Protocols/AppDependencies/HasAirQualityService.swift
+++ SampleApp/Protocols/AppDependencies/HasAirQualityService.swift
@@ -2,7 +2,7 @@
 //  HasAirQualityService.swift
 //  SampleApp
 //
-//  Created by Agnieszka K on 30.04.2018.
+//  Created by Bartosz Kucharski on 30.04.2018.
 //  Copyright © 2018 Bartosz Kucharski. All rights reserved.
 //
 
